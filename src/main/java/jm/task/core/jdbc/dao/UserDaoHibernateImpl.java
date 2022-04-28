e1059a2a789f3ad311e0a5a4d61f9f2d815990a2
Winginthedesert <69441486+Winginthedesert@users.noreply.github.com>
2022-04-28 11:52:26 -0700

    sout deleted. No logging

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import static jm.task.core.jdbc.util.Util.getSessionFactory;
 
 public class UserDaoHibernateImpl implements UserDao {
     private static Transaction transaction = null;
+    private static SessionFactory sessionFactory = getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
@@ -18,7 +20,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
 
             transaction = session.beginTransaction();
             String sql = "CREATE TABLE IF NOT EXISTS users " +
@@ -29,7 +31,6 @@ public class UserDaoHibernateImpl implements UserDao {
             session.createSQLQuery(sql).addEntity(User.class).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            System.out.println("Couldn't create the table: " + e.getMessage());
             if (transaction != null) {
                 transaction.rollback();
             }
@@ -39,14 +40,13 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
 
-        try (Session session = getSessionFactory().openSession();) {
+        try (Session session = sessionFactory.openSession();) {
 
             transaction = session.beginTransaction();
             String sql = "DROP TABLE IF EXISTS users";
             session.createSQLQuery(sql).addEntity(User.class).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            System.out.println("Couldn't delete the table: " + e.getMessage());
             if (transaction != null) {
                 transaction.rollback();
             }
@@ -56,13 +56,12 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
 
             transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
             transaction.commit();
         } catch (Exception e) {
-            System.out.println("Couldn't save user: " + e.getMessage());
             e.printStackTrace();
             if (transaction != null) {
                 transaction.rollback();
@@ -74,14 +73,13 @@ public class UserDaoHibernateImpl implements UserDao {
     public void removeUserById(long id) {
 
         User user;
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
 
             transaction = session.beginTransaction();
             user = session.find(User.class, id);
             session.remove(user);
             transaction.commit();
         } catch (Exception e) {
-            System.out.println("Couldn't delete user: " + e.getMessage());
             if (transaction != null) {
                 transaction.rollback();
             }
@@ -91,11 +89,10 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         String multipleQuery = "SELECT user FROM users user";
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             return session.createQuery(multipleQuery, User.class).getResultList();
 
         } catch (Exception e) {
-            System.out.println("Couldn't get users: " + e.getMessage());
             return null;
         }
     }
@@ -104,12 +101,11 @@ public class UserDaoHibernateImpl implements UserDao {
     public void cleanUsersTable() {
         String deleteQuery = "DELETE FROM users";
 
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createQuery(deleteQuery).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            System.out.println("Couldn't clear the table: " + e.getMessage());
             if (transaction != null) {
                 transaction.rollback();
             }
