06b1e05fd32208f7b3b124d3100d0bc27e5cef1a
Kirill:PC <Kirillbadyulin@gmail.com>
2022-04-28 11:13:54 -0700

    Hibernate update 4: no logging

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import static jm.task.core.jdbc.util.Util.getSessionFactory;
 
 public class UserDaoHibernateImpl implements UserDao {
     private static Transaction transaction = null;
+    private static SessionFactory sessionFactory = getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
@@ -18,7 +20,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
 
             transaction = session.beginTransaction();
             String sql = "CREATE TABLE IF NOT EXISTS users " +
@@ -39,7 +41,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
 
-        try (Session session = getSessionFactory().openSession();) {
+        try (Session session = sessionFactory.openSession();) {
 
             transaction = session.beginTransaction();
             String sql = "DROP TABLE IF EXISTS users";
@@ -56,7 +58,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
 
             transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
@@ -74,7 +76,7 @@ public class UserDaoHibernateImpl implements UserDao {
     public void removeUserById(long id) {
 
         User user;
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
 
             transaction = session.beginTransaction();
             user = session.find(User.class, id);
@@ -91,7 +93,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         String multipleQuery = "SELECT user FROM users user";
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             return session.createQuery(multipleQuery, User.class).getResultList();
 
         } catch (Exception e) {
@@ -104,7 +106,7 @@ public class UserDaoHibernateImpl implements UserDao {
     public void cleanUsersTable() {
         String deleteQuery = "DELETE FROM users";
 
-        try (Session session = getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createQuery(deleteQuery).executeUpdate();
             transaction.commit();
